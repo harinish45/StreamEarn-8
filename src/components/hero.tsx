65b4d57020841a6ffd0ad9752b53337f303e6f58
Harinish Sabapathi <harinishsv@gmail.com>
2025-12-16 17:41:41 +0000

    Try fixing this error: `Runtime Error: TypeError: Cannot read properties

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { placeholderImages } from "@/lib/placeholder-images";
+import placeholderImages from "@/lib/placeholder-images.json" with { type: "json" };
 
 export function Hero() {
   const heroImage = placeholderImages.heroImage;
