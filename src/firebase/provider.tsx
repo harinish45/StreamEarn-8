825d650531360a1b86bcf260f171a859719678bd
Harinish Sabapathi <harinishsv@gmail.com>
2026-01-03 10:44:59 +0000

    fix this error Application error: a client-side exception has occurred w

--- src/firebase/provider.tsx
+++ src/firebase/provider.tsx
@@ -4,12 +4,7 @@ import { createContext, useContext, useMemo } from 'react';
 import type { FirebaseApp } from 'firebase/app';
 import type { Auth } from 'firebase/auth';
 import type { Firestore } from 'firebase/firestore';
-import dynamic from 'next/dynamic';
-
-const FirebaseClientProvider = dynamic(() => import('./client-provider').then(mod => mod.FirebaseClientProvider), {
-  ssr: false,
-});
-
+import { initializeFirebase } from '.';
 
 export type FirebaseContextValue = {
   app: FirebaseApp;
@@ -26,10 +21,22 @@ export function FirebaseProvider({
 }: {
   children: React.ReactNode;
 }) {
+  const firebaseMemo = useMemo(initializeFirebase, []);
+
+  const contextValue = useMemo(() => {
+    if (!firebaseMemo) return undefined;
+    return {
+      app: firebaseMemo.app,
+      auth: firebaseMemo.auth,
+      firestore: firebaseMemo.firestore,
+    };
+  }, [firebaseMemo]);
+
+
   return (
-    <FirebaseClientProvider>
+    <FirebaseContext.Provider value={contextValue}>
       {children}
-    </FirebaseClientProvider>
+    </FirebaseContext.Provider>
   );
 }
 
