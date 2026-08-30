726c84445b7e46cf79a09848e526ae6acdd53762
Harinish <harinishsv@gmail.com>
2026-08-30 14:27:53 +0530

    security: remove unused remote image allowlist

--- next.config.js
+++ next.config.js
@@ -20,12 +20,6 @@ const nextConfig = {
       ],
     }];
   },
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'placehold.co', port: '', pathname: '/**' },
-      { protocol: 'https', hostname: 'picsum.photos', port: '', pathname: '/**' },
-    ],
-  },
 };
 
 module.exports = nextConfig;
