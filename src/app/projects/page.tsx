3904eb6a3ae9dc61db6aa0dbc0555b77535740d6
Harinish <harinishsv@gmail.com>
2026-09-01 00:18:03 +0530

    use single stable project command center

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,5 @@
-import CommandCenter from '@/components/projects/CommandCenter';
-import ProjectCreateBridge from '@/components/projects/project-create-bridge';
-import ProjectInteractionBridge from '@/components/projects/project-interaction-bridge';
-import IdeaSubmitBridge from '@/components/projects/idea-submit-bridge';
+import ProjectCommandCenterStable from '@/components/projects/ProjectCommandCenterStable';
 
-export default function ProjectsPage() {
-  return (
-    <>
-      <ProjectCreateBridge />
-      <ProjectInteractionBridge />
-      <IdeaSubmitBridge />
-      <CommandCenter />
-    </>
-  );
+export default function ProjectsPage(){
+  return <ProjectCommandCenterStable />;
 }
