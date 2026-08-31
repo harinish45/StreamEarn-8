0a83f5cf41df6d4d3bfaad9ab93b6507da6d57f5
Harinish <harinishsv@gmail.com>
2026-08-31 21:45:02 +0530

    show persistent ideas section in project center

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,13 @@
 import CommandCenter from '@/components/projects/CommandCenter';
 import ProjectCreateBridge from '@/components/projects/project-create-bridge';
+import ProjectIdeas from '@/components/projects/ProjectIdeas';
 
 export default function ProjectsPage() {
   return (
     <>
       <ProjectCreateBridge />
       <CommandCenter />
+      <ProjectIdeas />
     </>
   );
 }
