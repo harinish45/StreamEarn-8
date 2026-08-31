5a0fb05bb54108ec5e4b7ec6f83999bf81cbf72f
Harinish <harinishsv@gmail.com>
2026-08-31 23:25:00 +0530

    wire native idea submission bridge

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,14 @@
 import CommandCenter from '@/components/projects/CommandCenter';
 import ProjectCreateBridge from '@/components/projects/project-create-bridge';
 import ProjectInteractionBridge from '@/components/projects/project-interaction-bridge';
+import IdeaSubmitBridge from '@/components/projects/idea-submit-bridge';
 
 export default function ProjectsPage() {
   return (
     <>
       <ProjectCreateBridge />
       <ProjectInteractionBridge />
+      <IdeaSubmitBridge />
       <CommandCenter />
     </>
   );
