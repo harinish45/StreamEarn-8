7282322078c84b14962c9d7854c8caee29c3d613
Harinish <harinishsv@gmail.com>
2026-09-02 13:00:50 +0530

    Use verified curated internship radar only

--- src/app/internships/page.tsx
+++ src/app/internships/page.tsx
@@ -1,8 +1,2 @@
 import { OpportunityTypePage } from '@/components/opportunity-type-page';
-import { ScheduledOpportunityFeed } from '@/components/scheduled-opportunity-feed';
-
-export const dynamic = 'force-dynamic';
-
-export default function InternshipsPage(){
-  return <><OpportunityTypePage kind="Internship"/><div className="mx-auto w-full max-w-[1380px] px-3 pb-10 sm:px-5 lg:px-7"><ScheduledOpportunityFeed kind="Internship"/></div></>;
-}
+export default function InternshipsPage(){ return <OpportunityTypePage kind="Internship"/>; }
